094ba1fda71abfdbb5cfeb4409b4812e28ea7338
LWOS0x10 <lwos0x10@gmail.com>
2026-02-13 20:35:57 +0700

    GDT

---
+++ kernel/include/cpu/gdt.h
@@ -0,0 +1,24 @@
+#ifndef GDT_H
+#define GDT_H
+
+#include <stdint.h>
+
+struct gdt_entry {
+    uint16_t limit_low;
+    uint16_t base_low;
+    uint8_t  base_middle;
+    uint8_t  access;
+    uint8_t  granularity;
+    uint8_t  base_high;
+} __attribute__((packed));
+
+struct gdt_ptr {
+    uint16_t limit;
+    uint32_t base;
+} __attribute__((packed));
+
+extern void gdt_init();
+extern void gdt_set_gate(int num, uint32_t base, uint32_t limit, uint8_t access, uint8_t gran);
+extern void gdt_flush(uint32_t gdt_ptr_address);
+
+#endif
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,14 +1,16 @@
 #include <drivers/vga.h>
 #include <klib/kprintf.h>
+#include <cpu/gdt.h>
 
 void kernel_main(uint32_t mb2_magic, uint32_t mb2_info_ptr) {
         (void)mb2_magic;
         (void)mb2_info_ptr;
 
+        gdt_init();
+
         vga_init();
         vga_setcolor(VGA_COLOR_LIGHT_BROWN, VGA_COLOR_BLUE);
         vga_clear();
         vga_write("Welcome to LOS.\n");
-        kprintf("%c = %d; %s\n", 'x', 10, "Congrat!");
         for (;;) { __asm__ volatile("hlt"); }
 }
